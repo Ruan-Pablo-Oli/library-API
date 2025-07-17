ce66819798f92ec820119bf87e90aa70f125bf6c
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-17 15:57:00 -0300

    feat: Created entity Autor

---
+++ src/main/java/io/github/ruan_pablo_oli/library/model/Autor.java
@@ -0,0 +1,32 @@
+package io.github.ruan_pablo_oli.library.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="autor", schema = "public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column( name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column( name = "data_nascimento",nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade",nullable = false,length = 50)
+    private String nacionalidade;
+    
+
+}
