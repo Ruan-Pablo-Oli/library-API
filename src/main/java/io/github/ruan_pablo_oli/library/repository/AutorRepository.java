4ebd7df9121912e068387cab1c95d5f5daa74b44
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-28 22:56:20 -0300

    Created methods for filtring search using names and nationality

--- src/main/java/io/github/ruan_pablo_oli/library/repository/AutorRepository.java
+++ src/main/java/io/github/ruan_pablo_oli/library/repository/AutorRepository.java
@@ -3,9 +3,13 @@ package io.github.ruan_pablo_oli.library.repository;
 import io.github.ruan_pablo_oli.library.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
 
+    List<Autor> findByNomeIgnoreCaseContaining(String nome);
+    List<Autor> findByNacionalidadeIgnoreCaseContaining(String nacionalidade);
+    List<Autor> findByNomeIgnoreCaseContainingAndNacionalidadeIgnoreCaseContaining(String nome, String nacionalidade);
 }
--- src/main/java/io/github/ruan_pablo_oli/library/service/AutorService.java
+++ src/main/java/io/github/ruan_pablo_oli/library/service/AutorService.java
@@ -1,11 +1,11 @@
 package io.github.ruan_pablo_oli.library.service;
 
 
-import io.github.ruan_pablo_oli.library.controller.DTO.AutorDTO;
 import io.github.ruan_pablo_oli.library.model.Autor;
 import io.github.ruan_pablo_oli.library.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -29,7 +29,22 @@ public class AutorService {
     }
 
     public void deletarPorId(Autor  autor) {
-        autorRepository.deleteById(autor);
+        autorRepository.delete(autor);
 
+    }
+
+    public List<Autor> pesquisa(String nome,String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return autorRepository.findByNomeIgnoreCaseContainingAndNacionalidadeIgnoreCaseContaining(nome, nacionalidade);
+        }
+
+        if(nome != null){
+            return autorRepository.findByNomeIgnoreCaseContaining(nome);
+        }
+        if(nacionalidade != null){
+            return autorRepository.findByNacionalidadeIgnoreCaseContaining(nacionalidade);
+        }
+
+        return autorRepository.findAll();
     }
 }
