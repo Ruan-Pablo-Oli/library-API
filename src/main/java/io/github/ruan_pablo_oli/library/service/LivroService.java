3a74f137e332e6c367b1f3093fdb2e28c78ad0ff
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-08-23 10:46:04 -0300

    Implementing book search with interface specifications, create LivroSpecs isbn, title and genre

--- src/main/java/io/github/ruan_pablo_oli/library/service/LivroService.java
+++ src/main/java/io/github/ruan_pablo_oli/library/service/LivroService.java
@@ -1,11 +1,16 @@
 package io.github.ruan_pablo_oli.library.service;
 
 
+import io.github.ruan_pablo_oli.library.model.GeneroLivro;
 import io.github.ruan_pablo_oli.library.model.Livro;
 import io.github.ruan_pablo_oli.library.repository.LivroRepository;
+import io.github.ruan_pablo_oli.library.repository.specs.LivroSpecs;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +31,29 @@ public class LivroService {
 
     public void deleteLivro(Livro livro){
         livroRepository.delete(livro);
+    }
+
+
+    public List<Livro> pesquisa(String isbn, String titulo,String nomeAutor, GeneroLivro genero, Integer anoPublicacao){
+
+        // Select * from livro where isbn = :isbn and nomeAutor = :nomeAutor
+//        Specification<Livro> specs = Specification.where(LivroSpecs.isbnEqual(isbn)).and(LivroSpecs.tituloLike(titulo)).and(LivroSpecs.generoEqual(genero));
+
+        Specification<Livro> specs = (root, query, cb) -> cb.conjunction();
+        if(isbn != null){
+            // query = query and isbn = : isbn
+            specs = specs.and(LivroSpecs.isbnEqual(isbn));
+        }
+
+        if(titulo != null){
+            specs = specs.and(LivroSpecs.tituloLike(titulo));
+        }
+
+        if(genero != null){
+            specs = specs.and(LivroSpecs.generoEqual(genero));
+        }
+
+        return livroRepository.findAll(specs);
     }
 
 }
