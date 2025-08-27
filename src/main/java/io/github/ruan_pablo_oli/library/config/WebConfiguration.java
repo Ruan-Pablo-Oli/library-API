4f13da6ea078344f8c73d77014e3f1889752e446
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-08-27 16:06:05 -0300

    Adding spring security starter pack with formlogin using thymeleaf configured and httpbasic

---
+++ src/main/java/io/github/ruan_pablo_oli/library/config/WebConfiguration.java
@@ -0,0 +1,20 @@
+package io.github.ruan_pablo_oli.library.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
---
+++ src/main/java/io/github/ruan_pablo_oli/library/controller/LoginViewController.java
@@ -0,0 +1,14 @@
+package io.github.ruan_pablo_oli.library.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
+}
